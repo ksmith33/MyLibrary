04fd44c13b4a590adda0cfbb1962be370a029648
Keshyra <ksmith33@mail.umw.edu>
2023-09-14 18:58:37 -0400

    added basic pagination to the search results page

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -4,12 +4,11 @@ const BUTTON_TYPES = {
 	default: 'default',
 	listItem: 'listItem',
 	icon: 'icon'
-
 }
 
-function Button ({ children, buttonType,...otherProps}) {
+function Button ({ children, buttonType, pressed,...otherProps}) {
 	return (
-		<button className={ `button-container ${ BUTTON_TYPES[buttonType] }` }{ ...otherProps } > { children } </button>
+		<button className={ `button-container ${ BUTTON_TYPES[buttonType] } ${pressed ? 'pressed' : ''}` }{ ...otherProps } > { children } </button>
 	)
 }
 
--- src/components/search-results/search-results.component.jsx
+++ src/components/search-results/search-results.component.jsx
@@ -3,16 +3,31 @@ import { useSearchParams, Link } from "react-router-dom";
 import { fetchBooks } from "../../services/books";
 import "./search-results.styles.scss";
 import SearchBookListing from "../search-book-listing/search-book-listing.component";
+import Button from "../button/button.component";
 
 function SearchResults () {
 	const [searchParams] = useSearchParams();
 	const [searchResults, setSearchResults ] = useState([]);
+	//hook?
+	const [pageDetails, setPageDetails] = useState({
+		currentPage: 0,
+		pages: [],
+	});
+	const { currentPage, pages} = pageDetails;
+	const itemsPerPage = 15;
+	const startIndex = currentPage * itemsPerPage;
+	const endIndex = startIndex + itemsPerPage;
+	const page = searchResults.slice(startIndex, endIndex);
 	
 	useEffect (() => {
 		const search = async () => {
 			try {
 				const q = searchParams.get('q');
-				setSearchResults(await fetchBooks(q));
+				const results = await fetchBooks(q);
+				setSearchResults(results);
+				const totalPages = Math.ceil(results.length/itemsPerPage);
+				const pageArray = Array.from({length: totalPages}, (x, i) => i + 1)
+				setPageDetails({...pageDetails, pages: pageArray})
 			} catch (error) {
 				console.log(error)
 			}
@@ -20,14 +35,27 @@ function SearchResults () {
 		search();
 	}, [searchParams]);
 
+	function handlePageNumberClick (page) {
+		setPageDetails({...pageDetails, currentPage: page - 1});
+	}
+	
 	return (
-		<section>
-			<ul className="search-results-container">
+		<section className="search-results-container">
+			<ul>
 				{
-					searchResults.map(searchResult => {
+					page.map(searchResult => {
 						return (
 							<SearchBookListing book={ searchResult } />
 						)
+					})
+				}
+			</ul>
+			<ul className="page-numbers-list">
+				{
+					pages.map(page => {
+						return (
+							<li><Button buttonType = 'default' pressed = {page === currentPage + 1} onClick= {() => handlePageNumberClick(page)}>{page}</Button></li>
+						) 
 					})
 				}
 			</ul>
